803384b0ebdda84ccbf0a46201e4f5e1654fce87
ymz <577361548@qq.com>
2018-12-16 23:47:18 +0800

    检查js并且解决json遗留问题 待解决优化图片路径/图片模块结构加载不进来 Signed-off-by: ymz <577361548@qq.com>

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,37 +3,63 @@ require('styles/App.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+// let yeomanImage = require('../images/yeoman.png');
+let imagesDatas = require('json!../data/imagesData.json');
 
-// var imagesDatas = require('../data/imagesData.json');
-// function genImageURL (imagesDatasArr){
-//   for(var i = 0,j = imagesDatasArr.length; i<j;i++){
-//     var singleImageData = imagesDatasArr[i]
-//     singleImageData.genImageURL = require('.images/'+singleImageData.fileName)
-//     imagesDatasArr[i] = singleImageData;
-//   }
-//   return imagesDatasArr;
-// }
-// imagesDatas = genImageURL(imagesDatas);
-// console.log(imagesDatas);
+function genImageURL (imagesDatasArr){
+  for(var i = 0,j = imagesDatasArr.length; i<j;i++){
+    var singleImageData = imagesDatasArr[i]
+    singleImageData.imageURL = require('../images/'+singleImageData.fileName)
+    imagesDatasArr[i] = singleImageData;
+  }
+  return imagesDatasArr;
+}
+imagesDatas = genImageURL(imagesDatas);
+console.log(imagesDatas)
 
 
-class AppComponent extends React.Component {
+
+var imgFigure = React.createClass ({
+  render:function(){
+    return(
+      <figure>
+        <img src={this.props.data.imageURL} alt={this.props.data.title}/>
+        <figcaption>
+          <h2>{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+    );
+  }
+});
+
+
+class App extends React.Component {
   render() {
+    var controllerUnits = [],
+          imgFigures = [];
+          
+    imagesDatas.forEach((value,index) => {
+      imgFigures.push(<imgFigure data={value} key={'imgFigures'+index}/>)       
+    });
+    
     return (
       // <div className="index">
       //   <img src={yeomanImage} alt="Yeoman Generator" />
       //   <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
       // </div>
       <section className = "stage">
-        <section className = "img-sec"></section>
-        <nav className = "controller-nav"></nav>
+        <section className = "img-sec">
+          {imgFigures}
+        </section>
+        <nav className = "controller-nav">
+          {controllerUnits}
+        </nav>
       </section>
     );
   }
 }
 
-AppComponent.defaultProps = {
+App.defaultProps = {
 };
 
-export default AppComponent;
+export default App;
