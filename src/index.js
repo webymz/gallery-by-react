95b4f829955af4c2cb82dc454b052c86883e9cb2
ymz <577361548@qq.com>
2019-03-23 16:52:46 +0800

    iii

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import 'core-js/fn/object/assign';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GalleryByReactApp from './components/Main';
-
-// Render the main component into the dom
-ReactDOM.render(<GalleryByReactApp />, document.getElementById('content'));
